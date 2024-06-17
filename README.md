6a4c0eb2cd105673bd7365fee1d292f6d9cb9ae8
kensuke <kensuken@andrew.cmu.edu>
2024-06-17 17:11:39 -0400

    first push

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # latent-safety
 # latent-safety
+
+
+# generating image-based pickle files:
+conda activate safe-rl
+python scripts/generate_data.py
+
+# training models
+python scripts.train_classifier.py
+python scripts.train_classifier_img.py
+python scripts.train_dynamics.py
+python scripts.train_dynamics_img.py
+
+
+# getting grid-based solution
+conda activate odp
+python scripts/toy.py
---
+++ networks/cnn.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.parametrizations import spectral_norm
+import numpy as np
+import math
+from networks.mlp import MLP
+
+class ConvEncoderMLP(nn.Module):
+    def __init__(
+        self,
+        input_shape,
+        depth=32,
+        act="SiLU",
+        norm=True,
+        kernel_size=4,
+        minres=4,
+        out_dim = 0,
+        in_dim = 0,
+        hidden_dim= 256,
+        hidden_layer=1,
+    ):
+        super(ConvEncoderMLP, self).__init__()
+        self.in_dim = in_dim
+        self.enc = ConvEncoder(input_shape, depth, act, norm, kernel_size, minres)
+        self.mlp = MLP(self.enc.outdim+in_dim, out_dim, hidden_dim, hidden_layer = hidden_layer)
+
+    def forward(self, obs, state=None):
+        enc = self.enc(obs)
+        if self.in_dim != 0:
+            inp = torch.cat([enc, state.unsqueeze(1)], dim=1)
+        else:
+            inp = enc
+        return self.mlp(inp)
+
+
+class ConvEncoder(nn.Module):
+    def __init__(
+        self,
+        input_shape,
+        depth=32,
+        act="SiLU",
+        norm=True,
+        kernel_size=4,
+        minres=4,
+    ):
+        super(ConvEncoder, self).__init__()
+        act = getattr(torch.nn, act)
+        h, w, input_ch = input_shape
+        stages = int(np.log2(h) - np.log2(minres))
+        in_dim = input_ch
+        out_dim = depth
+        layers = []
+        for i in range(stages):
+            layers.append(
+                Conv2dSamePad(
+                    in_channels=in_dim,
+                    out_channels=out_dim,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    bias=False,
+                )
+            )
+            if norm:
+                layers.append(ImgChLayerNorm(out_dim))
+            layers.append(act())
+            in_dim = out_dim
+            out_dim *= 2
+            h, w = h // 2, w // 2
+
+        self.outdim = out_dim // 2 * h * w
+        self.layers = nn.Sequential(*layers)
+        self.layers.apply(weight_init)
+
+    def forward(self, obs):
+        obs -= 0.5
+        # (batch, time, h, w, ch) -> (batch * time, h, w, ch)
+        x = obs.reshape((-1,) + tuple(obs.shape[-3:]))
+        # (batch * time, h, w, ch) -> (batch * time, ch, h, w)
+        x = x.permute(0, 3, 1, 2)
+        x = self.layers(x)
+        # (batch * time, ...) -> (batch * time, -1)
+        x = x.reshape([x.shape[0], np.prod(x.shape[1:])])
+        # (batch * time, -1) -> (batch, time, -1)
+        return x.reshape(list(obs.shape[:-3]) + [x.shape[-1]])
+
+
+class Conv2dSamePad(torch.nn.Conv2d):
+    def calc_same_pad(self, i, k, s, d):
+        return max((math.ceil(i / s) - 1) * s + (k - 1) * d + 1 - i, 0)
+
+    def forward(self, x):
+        ih, iw = x.size()[-2:]
+        pad_h = self.calc_same_pad(
+            i=ih, k=self.kernel_size[0], s=self.stride[0], d=self.dilation[0]
+        )
+        pad_w = self.calc_same_pad(
+            i=iw, k=self.kernel_size[1], s=self.stride[1], d=self.dilation[1]
+        )
+
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(
+                x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2]
+            )
+
+        ret = F.conv2d(
+            x,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+        return ret
+
+class ImgChLayerNorm(nn.Module):
+    def __init__(self, ch, eps=1e-03):
+        super(ImgChLayerNorm, self).__init__()
+        self.norm = torch.nn.LayerNorm(ch, eps=eps)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 3, 1)
+        x = self.norm(x)
+        x = x.permute(0, 3, 1, 2)
+        return x
+
+def weight_init(m):
+    if isinstance(m, nn.Linear):
+        in_num = m.in_features
+        out_num = m.out_features
+        denoms = (in_num + out_num) / 2.0
+        scale = 1.0 / denoms
+        std = np.sqrt(scale) / 0.87962566103423978
+        nn.init.trunc_normal_(
+            m.weight.data, mean=0.0, std=std, a=-2.0 * std, b=2.0 * std
+        )
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        space = m.kernel_size[0] * m.kernel_size[1]
+        in_num = space * m.in_channels
+        out_num = space * m.out_channels
+        denoms = (in_num + out_num) / 2.0
+        scale = 1.0 / denoms
+        std = np.sqrt(scale) / 0.87962566103423978
+        nn.init.trunc_normal_(
+            m.weight.data, mean=0.0, std=std, a=-2.0 * std, b=2.0 * std
+        )
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.LayerNorm):
+        m.weight.data.fill_(1.0)
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
