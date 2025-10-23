b8cb53af542fcb5db65d761f44cc20f41c8153ae
Praveen Selvaraj <praveen.beatle@gmail.com>
2025-10-23 21:01:33 +0100

    docker with wandb and arm64 build

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -3,3 +3,6 @@ IMAGE_NAME="latent_safety"
  
 docker build -t $IMAGE_NAME -f Dockerfile .. 
 
+# docker buildx create --use
+# docker buildx build --platform linux/arm64 -t latent_safety -f Dockerfile .. 
+# docker buildx build --platform linux/arm64 -t latent_safety --load -f Dockerfile ..
