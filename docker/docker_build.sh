b5b9127bbbd6802a3ef8915dd8b03a11a1cd759a
Praveen Selvaraj <praveen.beatle@gmail.com>
2025-10-23 23:24:10 +0100

    torch with cuda for aarch64

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 IMAGE_NAME="latent_safety"
  
-docker build -t $IMAGE_NAME -f Dockerfile .. 
+# docker build -t $IMAGE_NAME -f Dockerfile .. 
 
 # docker buildx create --use
-# docker buildx build --platform linux/arm64 -t latent_safety -f Dockerfile .. 
-# docker buildx build --platform linux/arm64 -t latent_safety --load -f Dockerfile ..
+# docker buildx build --platform linux/arm64 -t $IMAGE_NAME -f Dockerfile .. 
+docker buildx build --platform linux/arm64 -t $IMAGE_NAME --load -f Dockerfile ..
