e93c0eb8a55d7e27e991cb3d273c82ea3688c598
Praveen Selvaraj <praveen.beatle@gmail.com>
2026-01-27 00:53:02 +0000

    docker and slurm

--- slurm/train_dinowm_classifier_slurm.sh
+++ slurm/train_dinowm_classifier_slurm.sh
@@ -25,7 +25,7 @@ container="${data_dir}/container/latent_safety_arm64.sif"
 
 # Training config (weights and checkpoints on scratch due to limited home storage)
 HDF5_FILE="${data_dir}/cubes_push_labeled_combined_v3.h5"
-DATASET_STATS="${data_dir}/cubes_push_labeled_combined_v3_stats.json"
+DATASET_STATS="${data_dir}/cubes_push_labeled_dataset_stats.json"
 DECODER_CHECKPOINT="${data_dir}/dino3_decoder_checkpoints/best_decoder.pth"
 WM_CHECKPOINT="${data_dir}/dino3_wm_checkpoints/best_wm.pth"
 CHECKPOINT_DIR="${data_dir}/dino3_classifier_checkpoints"
